2d6259895fbca4907a5484604795bb95da5815bf
james <dev@dysomni.com>
2020-10-21 23:06:25 -0400

    second commit 👀

--- lib/sneaky_import.rb
+++ lib/sneaky_import.rb
@@ -1,24 +1,25 @@
 require "sneaky_import/version"
+require 'active_support/inflector'
 
 module SneakyImport
 
-  def sneaky_import(*args)
+  def self.sneaky_import(*args)
     result = Rails.logger.silence { import(*args) }
     sneaky_log(result)
   end
 
-  def sneaky_import!(*args)
+  def self.sneaky_import!(*args)
     result = Rails.logger.silence { import!(*args) }
     sneaky_log(result)
   end
 
-  def sneaky_log(result)
+  def self.sneaky_log(result)
     log_color = result.failed_instances.length == 0 ? :light_green : :red
     Rails.logger.info "#{to_s.pluralize} >>> #{result.ids.length} imported / #{result.failed_instances.length} failed <<<".colorize(log_color)
     return result
   end
 
-  def sneaky_to_a
+  def self.sneaky_to_a
     result = Rails.logger.silence { all.to_a }
     Rails.logger.info "Selected #{result.count} #{to_s.pluralize(result.count)}".colorize(:light_blue)
     return result
@@ -26,4 +27,4 @@ module SneakyImport
 
 end
 
-ActiveRecord::Base.send :extend, SneakyImport
+ActiveRecord::Base.send :include, SneakyImport
