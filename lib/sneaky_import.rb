9054386dcd99af5c6323372c9c76d4d54337b96a
James Brock <jbrock3852@gmail.com>
2020-10-21 23:26:55 -0400

    fixed cached error

--- lib/sneaky_import.rb
+++ lib/sneaky_import.rb
@@ -3,23 +3,23 @@ require 'active_support/inflector'
 
 module SneakyImport
 
-  def self.sneaky_import(*args)
+  def sneaky_import(*args)
     result = Rails.logger.silence { import(*args) }
     sneaky_log(result)
   end
 
-  def self.sneaky_import!(*args)
+  def sneaky_import!(*args)
     result = Rails.logger.silence { import!(*args) }
     sneaky_log(result)
   end
 
-  def self.sneaky_log(result)
+  def sneaky_log(result)
     log_color = result.failed_instances.length == 0 ? :light_green : :red
     Rails.logger.info "#{to_s.pluralize} >>> #{result.ids.length} imported / #{result.failed_instances.length} failed <<<".colorize(log_color)
     return result
   end
 
-  def self.sneaky_to_a
+  def sneaky_to_a
     result = Rails.logger.silence { all.to_a }
     Rails.logger.info "Selected #{result.count} #{to_s.pluralize(result.count)}".colorize(:light_blue)
     return result
@@ -27,4 +27,4 @@ module SneakyImport
 
 end
 
-ActiveRecord::Base.send :include, SneakyImport
+ActiveRecord::Base.send :extend, SneakyImport
